c3cc3c1db40333310981a8b489f1b3713da68075
Carlton Caven <carlcaven@gmail.com>
2018-05-23 12:49:33 -0700

    pa&closure

---
+++ Iterators/PA&Closure.js
@@ -0,0 +1,56 @@
+// Type JavaScript here and click "Run Code" or press Ctrl + s
+console.log("Hello, world!");
+
+// CHALLENGE 1
+
+function sumFunc(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+// Uncomment the lines below to test your work
+const array = [1, 2, 3, 4];
+console.log(sumFunc(array)); // -> should log 10
+
+function returnIterator(arr) {
+  let i = 0;
+  function myIterator() {
+    const ele = arr[i];
+    i++;
+    return ele;
+  }
+  return myIterator;
+}
+
+// Uncomment the lines below to test your work
+const array2 = ["a", "b", "c", "d"];
+const myIterator = returnIterator(array2);
+console.log(myIterator()); // -> should log 'a'
+console.log(myIterator()); // -> should log 'b'
+console.log(myIterator()); // -> should log 'c'
+console.log(myIterator()); // -> should log 'd'
+
+// CHALLENGE 2
+
+function nextIterator(arr) {
+  // YOUR CODE HERE
+  let i = 0;
+  const iteratorWithNext = {
+    next: function() {
+      const ele = arr[i];
+      i++;
+      return ele;
+    }
+  };
+  return iteratorWithNext;
+}
+
+// Uncomment the lines below to test your work
+const array3 = [1, 2, 3];
+const iteratorWithNext = nextIterator(array3);
+console.log(iteratorWithNext.next()); // -> should log 1
+console.log(iteratorWithNext.next()); // -> should log 2
+console.log(iteratorWithNext.next()); // -> should log 3
